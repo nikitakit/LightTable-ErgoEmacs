c08f856dfd6538aa9832030f5ebfe0235d17e1f3
Nikita Kitaev <nikitakit@gmail.com>
2014-01-08 14:45:21 -0800

    Add compiled plugin.js too

---
+++ plugin.js
@@ -0,0 +1,15 @@
+goog.provide("lt.plugins.ergoemacs");
+goog.require("cljs.core");
+goog.require("lt.util.cljs");
+goog.require("lt.util.cljs");
+goog.require("lt.objs.command");
+goog.require("lt.objs.command");
+goog.require("lt.objs.editor.pool");
+goog.require("lt.objs.editor.pool");
+goog.require("lt.objs.editor");
+goog.require("lt.objs.editor");
+goog.require("lt.object");
+goog.require("lt.object");
+lt.objs.command.command.call(null, cljs.core.PersistentArrayMap.fromArray([new cljs.core.Keyword(null, "command", "command", 1964298941), new cljs.core.Keyword(null, "ergoemacs.test", "ergoemacs.test", 1365183486), new cljs.core.Keyword(null, "desc", "desc", 1016984067), "ErgoEmacs: test", new cljs.core.Keyword(null, "exec", "exec", 1017031683), function() {
+  return null
+}], true));
